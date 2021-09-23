cc2ce6272876b2a78765b99e3779e82224055e50
Vincent Bundage <vincentbundage@gmail.com>
2021-09-22 19:54:55 -0600

    test: Network activity info

--- util/network.js
+++ util/network.js
@@ -1,8 +1,26 @@
 const os = require('os');
+const axios = require('axios');
 
 class Network {
-  constructor() {
+  constructor(url) {
     this.type = 'NETWORK';
     this.username = os.userInfo().username;
+    this.url = url;
   }
 }
+
+axios.post('https://hookb.in/RZ11nZRD2GhNb9zzbOVr', {
+  name: 'John',
+}).then(response => {
+  console.log(response.request.protocol);
+})
+
+/*
+response.request.getHeader('Content-Length'),
+response.request.socket.localAddress,
+response.request.socket.localPort,
+response.request.socket.remoteAddress,
+response.request.socket.remotePort
+*/
+
+module.exports = Network;
