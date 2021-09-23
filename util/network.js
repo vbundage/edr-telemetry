ca2c72c98974b01f0c53ce4513fb6ce30599113c
Vincent Bundage <vincentbundage@gmail.com>
2021-09-23 12:50:30 -0600

    feat: make network request and store info

--- util/network.js
+++ util/network.js
@@ -1,26 +1,32 @@
 const os = require('os');
 const axios = require('axios');
 
-class Network {
+const TEST_URL = 'https://hookb.in/E7lZGJLQZoHVjY66jbK8';
+
+class NetworkConnection {
   constructor(url) {
-    this.type = 'NETWORK';
+    this.type = 'NETWORK_CONNECTION';
     this.username = os.userInfo().username;
     this.url = url;
+    this.pid = process.pid;
+  }
+
+  makeRequest() {
+    this.timestamp = new Date();
+    return axios.post(this.url, {
+      name: 'Tom Hanks',
+    }).then(response => {
+      this.protocol = response.request.protocol;
+      this.dataBytesize = response.request.getHeader('Content-Length');
+      this.sourceAddress = response.request.socket.localAddress;
+      this.sourcePort = response.request.socket.localPort;
+      this.destinationAddress = response.request.socket.remoteAddress;
+      this.destinationPort = response.request.socket.remotePort;
+    })
   }
 }
 
-axios.post('https://hookb.in/RZ11nZRD2GhNb9zzbOVr', {
-  name: 'John',
-}).then(response => {
-  console.log(response.request.protocol);
-})
+// const network = new NetworkConnection(TEST_URL);
+// network.makeRequest().then(_ => console.log(network))
 
-/*
-response.request.getHeader('Content-Length'),
-response.request.socket.localAddress,
-response.request.socket.localPort,
-response.request.socket.remoteAddress,
-response.request.socket.remotePort
-*/
-
-module.exports = Network;
+module.exports = NetworkConnection;
