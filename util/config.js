1c6b966c1485cdada3bf0dbd2349adce4fa1f1d7
Vincent Bundage <vincentbundage@gmail.com>
2021-09-23 19:53:24 -0600

    feat: configuration file and processing

---
+++ util/config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { readFile } = require('fs/promises');
+const Process = require('./process');
+const Network = require('./network');
+const File = require('./file');
+
+const processConfig = async configPath => {
+  const fileConfig = JSON.parse(await readFile(configPath, { encoding: 'utf-8' }));
+  const operations = fileConfig.operations.map(op => {
+    if (op.operationType === 'process') {
+      return new Process(op.command)
+    }
+    if (op.operationType === 'file') {
+      return new File(
+        path.basename(op.filename),
+        op.fileAction,
+        op.content !== undefined ? op.content : undefined,
+        path.dirname(op.filename) === '.' ? undefined : path.dirname(op.filename)
+      );
+    }
+    if (op.operationType === 'network') {
+      return new Network(op.url);
+    }
+    return null;
+  });
+  return operations;
+};
+
+module.exports = {
+  processConfig,
+};
