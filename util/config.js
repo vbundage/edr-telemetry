3fd2a1561a951223956a673c2dee14946dda4887
Vincent Bundage <vincentbundage@gmail.com>
2021-09-24 16:00:25 -0600

    refactor: util/config separate mapping logic

--- util/config.js
+++ util/config.js
@@ -4,25 +4,23 @@ const Process = require('../lib/process');
 const Network = require('../lib/network');
 const File = require('../lib/file');
 
+const mapOperationToActivity = op => {
+  if (op.operationType === 'process') return new Process(op.command)
+  if (op.operationType === 'file') {
+    return new File(
+      path.basename(op.filename),
+      op.fileAction,
+      op.content !== undefined ? op.content : undefined,
+      path.dirname(op.filename) === '.' ? undefined : path.dirname(op.filename)
+    );
+  }
+  if (op.operationType === 'network') return new Network(op.url, op.message);
+  return null;
+};
+
 const processConfig = async configPath => {
   const fileConfig = JSON.parse(await readFile(configPath, { encoding: 'utf-8' }));
-  const operations = fileConfig.operations.map(op => {
-    if (op.operationType === 'process') {
-      return new Process(op.command)
-    }
-    if (op.operationType === 'file') {
-      return new File(
-        path.basename(op.filename),
-        op.fileAction,
-        op.content !== undefined ? op.content : undefined,
-        path.dirname(op.filename) === '.' ? undefined : path.dirname(op.filename)
-      );
-    }
-    if (op.operationType === 'network') {
-      return new Network(op.url, op.message);
-    }
-    return null;
-  });
+  const operations = fileConfig.operations.map(mapOperationToActivity);
   return operations;
 };
 
