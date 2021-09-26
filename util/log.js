9bff389cf6bf176974e37bf575c434ca0f3059b0
Vincent Bundage <vincentbundage@gmail.com>
2021-09-26 11:44:51 -0600

    refactor: writing log as a function

--- util/log.js
+++ util/log.js
@@ -1,21 +1,26 @@
 const fsUtil = require('./fs');
 
 const LOG_PATH = './logs'
-const logActivity = async operations => {
-  try {
-    const logName = `/${(new Date()).toISOString()}-log.json`;
-    await fsUtil.writeFile(LOG_PATH + logName, JSON.stringify([], null, '\t'));
-    for (const operation of operations) {
+
+const writeToLog = async (operations, logName) => {
+  for (const operation of operations) {
+    try {
       await operation.startActivity();
       const readObj = JSON.parse(await fsUtil.readFile(LOG_PATH + logName));
       const writeObj = {}
       writeObj[`${operation.type}`] = operation;
       readObj.push(writeObj);
       await fsUtil.writeFile(LOG_PATH + logName, JSON.stringify(readObj, null, '\t'));
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    throw error;
   }
+};
+
+const logActivity = async operations => {
+  const logName = `/${(new Date()).toISOString()}-log.json`;
+  await fsUtil.writeFile(LOG_PATH + logName, JSON.stringify([], null, '\t'));
+  await writeToLog(operations, logName);
 };
 
 module.exports = {
